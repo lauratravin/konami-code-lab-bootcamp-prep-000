9c46695731f5f2686e8b68c3385276f7e9fa988f
Laura M. Travin <laura.travin@gmail.com>
2017-11-28 01:03:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
-document.body.addEventListener('keydown', init(e));
+
+init(){
+
+document.body.addEventListener('keydown', function(e));
 
 
 
-function init(e) {
-  const key = parseInt( e.detail || e.which);
+function function(e) {
+        const key = parseInt( e.detail || e.which);
 
-  if (key === code[index]) {
-            index++;
+        if (key === code[index]) {
+                  index++;
 
-            if (index === code.length) {
-              alert("Hurray!");
+                  if (index === code.length) {
+                    alert("Hurray!");
 
-              index = 0;
-            }
-  } else {
-    index = 0;
-  }
+                    index = 0;
+                  }
+        } else {
+          index = 0;
+        }
 
+      }
 }
+init();
