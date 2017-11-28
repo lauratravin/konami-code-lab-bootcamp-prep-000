f050272574b2369cbdaaa822463e01b817d3b477
Laura M. Travin <laura.travin@gmail.com>
2017-11-28 00:43:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ document.body.addEventListener('keydown', init(e));
 
 
 function init(e) {
-  const key = parseInt(e.detail || e.which);
+  const key = parseInt( e.detail || e.which);
 
   if (key === code[index]) {
             index++;
