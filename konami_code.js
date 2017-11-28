e4e251fedc6f2bf53be48c2495d19a99c876493e
Laura M. Travin <laura.travin@gmail.com>
2017-11-28 01:29:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
+var body = document.body;
 
+document.body.addEventListener('keydown', init(e));
 
 
 
@@ -22,7 +24,5 @@ function init(e){
 
       }
 
-      var body = document.body;
 
-      body.addEventListener('keydown', init(e));
 //NO ANDA   TypeError: Cannot read property 'which' of undefined
