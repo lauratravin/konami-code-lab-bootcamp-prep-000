3a043767716ca10355623777869e90a14d6b3a12
Laura M. Travin <laura.travin@gmail.com>
2017-11-28 01:34:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 
-document.body.addEventListener('keydown', init(e){
+document.body.addEventListener('keydown', init(){
         const key = parseInt(e.details || e.which);
 
         if (key === code[index]) {
