f270efb2b24d4def9267fc88cd84fff95620f6b7
Laura M. Travin <laura.travin@gmail.com>
2017-11-28 01:45:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,29 +1,10 @@
-// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-// let index = 0;
-//
-// document.body.addEventListener('keydown', init(){
-//         const key = parseInt(e.details || e.which);
-//
-//         if (key === code[index]) {
-//                   index++;
-//
-//                   if (index === code.length) {
-//                     alert("Hurray!");
-//
-//                     index = 0;
-//                   }
-//         } else {
-//           index = 0;
-//         }
-//
-//       });
 
 
 //NO ANDA   TypeError: Cannot read property 'which' of undefined
 
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
-document.body.addEventListener('keydown', init):
+document.body.addEventListener('keydown',init,false):
 
 
 funtion init(){
